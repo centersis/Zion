a4a130952a1ddcc880c6a2f2c6cdc41e30f20609
Pablo Vanni <pablovanni@gmail.com>
2014-09-24 08:37:30 -0400

    Correções básicas

--- Lib/Zion/ClassLoader/Loader.class.php
+++ Lib/Zion/ClassLoader/Loader.class.php
@@ -18,10 +18,10 @@ use \Zion\ClassLoader\UniversalClassLoader;
 class Loader
 {
 
-    public $LIBRARY_PATH = 'C:/xampp/htdocs/Zion/Lib';
-    public $SUFIXES = array("", ".vo", ".class", ".interface");
+    private $frameworkBase;
+    private $aplicacaoBase;    
 
-    public function __construct()
+    public function inicio()
     {
         require_once "UniversalClassLoader.php";
 
@@ -29,6 +29,40 @@ class Loader
         $loader->registerNamespace("Zion", $this->LIBRARY_PATH);
         $loader->registerNamespace("Teste", 'C:/xampp/htdocs/');
         $loader->registerSufixes($this->SUFIXES);
-        $loader->register();        
+        $loader->register();
     }
+    
+    public function getFrameworkBase()
+    {
+        return $this->frameworkBase;
+    }
+
+    public function setFrameworkBase($frameworkBase)
+    {
+        $this->frameworkBase = $frameworkBase;
+        return $this;
+    }
+
+    public function getAplicacaoBase()
+    {
+        return $this->aplicacaoBase;
+    }
+
+    public function setAplicacaoBase($aplicacaoBase)
+    {
+        $this->aplicacaoBase = $aplicacaoBase;
+        return $this;
+    }
+
+    public function getSufixos()
+    {
+        return $this->sufixos;
+    }
+
+    public function setSufixos(array $sufixos)
+    {
+        $this->sufixos = $sufixos;
+        return $this;
+    }
+
 }
