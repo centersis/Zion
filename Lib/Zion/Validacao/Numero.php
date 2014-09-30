3968c128fccbb8d482f3c695156f9b78a7318de7
Feliphe <feliphe@braconsultoria.com.br>
2014-09-30 16:29:15 -0400

    Adição de documentação à classe \Zion\Validacao\Numero().

--- Lib/Zion/Validacao/Numero.php
+++ Lib/Zion/Validacao/Numero.php
@@ -95,10 +95,9 @@ class Numero
     public function isFloat($numero)
     {
         if (preg_match('/[0-9]{1,3}[\.|,][0-9]{1,2}$/', $numero) and is_numeric($numero)) {
-            return (is_float($numero));
+            return true;
         } else {
             return false;
         }
     }
-
 }
