1758e0e0b4a0b9c142f9d267d9a46260068112f9
Feliphe <feliphe@braconsultoria.com.br>
2014-09-30 16:26:31 -0400

    Criação da classe de exceção \Zion\Form\Exception\FormInvalidArgumentException() para tratamento de exceções dos formulários.

---
+++ Lib/Zion/Form/Exception/FormInvalidArgumentException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Zion\Form\Exception\FormInvalidArgumentException()
+ * 
+ * @author Feliphe Bueno - feliphezion@gmail.com
+ * @since 30/09/2014
+ * @version 1.0
+ * @copyright 2014
+ * 
+ *  Base InvalidArgumentException for the Form component.
+ */
+
+
+namespace Zion\Form\Exception;
+
+class FormInvalidArgumentException extends InvalidArgumentException implements ExceptionInterface
+{
+}
