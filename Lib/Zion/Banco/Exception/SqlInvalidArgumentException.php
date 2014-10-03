67b0c6175d37e997599f78a9d38cdd6de1d4a848
Feliphe <feliphe@braconsultoria.com.br>
2014-10-03 15:04:33 -0400

    Criação da classe de tratamento de Sql Exceptions Zion\Banco\Exception\SqlInvalidArgumentException(var)

---
+++ Lib/Zion/Banco/Exception/SqlInvalidArgumentException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Zion\Banco\Exception\SqlInvalidArgumentException()
+ * 
+ * @author Feliphe Bueno - feliphezion@gmail.com
+ * @since 30/09/2014
+ * @version 1.0
+ * @copyright 2014
+ * 
+ *  Sql InvalidArgumentException.
+ */
+
+
+namespace Zion\Banco\Exception;
+
+class SqlInvalidArgumentException extends InvalidArgumentException implements ExceptionInterface
+{
+}
