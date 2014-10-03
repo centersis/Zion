d02138dec618016cc9aefca79ca1b513d502ca20
Feliphe <feliphe@braconsultoria.com.br>
2014-10-03 15:05:10 -0400

    Criação da classe de tratamento de Sql Exceptions Zion\Banco\Exception\SqlException

---
+++ Lib/Zion/Banco/Exception/SqlException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Zion\Banco\Exception\SqltException()
+ * 
+ * @author Feliphe Bueno - feliphezion@gmail.com
+ * @since 03/10/2014
+ * @version 2.0
+ * @copyright 2014
+ * 
+ *  Sql Exception.
+ */
+
+namespace Zion\Banco\Exception;
+
+class SqlException extends RuntimeException implements ExceptionInterface
+{
+}
