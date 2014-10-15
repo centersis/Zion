726d75ee051289c26c8d6aa4dae8c949dd5570b1
Feliphe <feliphe@braconsultoria.com.br>
2014-10-15 10:36:25 -0400

    Criação da classe Zion\Form\FormInputCnpj().

---
+++ Lib/Zion/Form/FormInputCnpj.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Zion\Form;
+
+use \Zion\Form\Exception\FormException as FormException;
+
+class FormInputCnpj extends FormBasico
+{
+
+    private $tipoBase;
+    private $acao;
+    private $maximoCaracteres;
+    private $minimoCaracteres;
+    private $obrigatorio;
+    private $placeHolder;
+
+    public function __construct($acao, $nome, $identifica, $obrigatorio)
+    {
+        $this->tipoBase = 'cnpj';
+        $this->acao = $acao;
+        $this->setNome($nome);
+        $this->setId($nome);
+        $this->setIdentifica($identifica);
+        $this->setObrigarorio($obrigatorio);
+    }
+
+    public function getTipoBase()
+    {
+        return $this->tipoBase;
+    }
+
+    public function getAcao()
+    {
+        return $this->acao;
+    }
+
+    public function setMaximoCaracteres($maximoCaracteres)
+    {
+        if (is_numeric($maximoCaracteres)) {
+
+            if (isset($this->minimoCaracteres) and ( $maximoCaracteres < $this->minimoCaracteres)) {
+                throw new FormException("maximoCaracteres nao pode ser menor que minimoCaracteres.");
+            }
+
+            $this->maximoCaracteres = $maximoCaracteres;
+            return $this;
+        } else {
+            throw new FormException("maximoCaracteres: Valor nao numerico.");
+        }
+    }
+
+    public function getMaximoCaracteres()
+    {
+        return $this->maximoCaracteres;
+    }
+
+    public function setMinimoCaracteres($minimoCaracteres)
+    {
+        if (is_numeric($minimoCaracteres)) {
+
+            if (isset($this->maximoCaracteres) and ( $minimoCaracteres > $this->maximoCaracteres)) {
+                throw new FormException("minimoCaracteres nao pode ser maior que maximoCaracteres.");
+            }
+
+            $this->minimoCaracteres = $minimoCaracteres;
+            return $this;
+        } else {
+            throw new FormException("minimoCaracteres: Valor nao numerico.");
+        }
+    }
+
+    public function getMinimoCaracteres()
+    {
+        return $this->minimoCaracteres;
+    }
+
+    public function setObrigarorio($obrigatorio)
+    {
+        if (is_bool($obrigatorio)) {
+            $this->obrigatorio = $obrigatorio;
+            return $this;
+        } else {
+            throw new FormException("obrigatorio: Valor nao booleano");
+        }
+    }
+
+    public function getObrigatorio()
+    {
+        return $this->obrigatorio;
+    }
+
+    public function setPlaceHolder($placeHolder)
+    {
+        if (!empty($placeHolder)) {
+            $this->placeHolder = $placeHolder;
+            return $this;
+        } else {
+            throw new FormException("placeHolder: Nenhum valor informado");
+        }
+    }
+
+    public function getPlaceHolder()
+    {
+        return $this->placeHolder;
+    }
+
+    /**
+     * Sobrecarga de Metodos Básicos
+     */
+    public function setId($id)
+    {
+        parent::setId($id);
+        return $this;
+    }
+
+    public function setNome($nome)
+    {
+        parent::setNome($nome);
+        return $this;
+    }
+
+    public function setIdentifica($identifica)
+    {
+        parent::setIdentifica($identifica);
+        return $this;
+    }
+
+    public function setValor($valor)
+    {
+        parent::setValor($valor);
+        return $this;
+    }
+
+    public function setValorPadrao($valorPadrao)
+    {
+        parent::setValorPadrao($valorPadrao);
+        return $this;
+    }
+
+    public function setDisabled($disabled)
+    {
+        parent::setDisabled($disabled);
+        return $this;
+    }
+
+    public function setComplemento($complemento)
+    {
+        parent::setComplemento($complemento);
+        return $this;
+    }
+
+    public function setAtributos($atributos)
+    {
+        parent::setAtributos($atributos);
+        return $this;
+    }
+
+    public function setClassCss($classCss)
+    {
+        parent::setClassCss($classCss);
+        return $this;
+    }
+
+}
