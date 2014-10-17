9ae69c019a8af58dc7b00c0a711b4b6807dbf2c8
Feliphe <feliphe@braconsultoria.com.br>
2014-10-17 11:14:43 -0400

    Criação da classe \Pixel\Form\FomInputEmail().

---
+++ Lib/Pixel/Form/FormInputEmail.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Pixel\Form;
+
+class FormInputEmail extends \Zion\Form\FormInputEmail
+{
+
+    private $iconFA;
+    private $toolTipMsg;
+    private $emColunaDeTamanho;
+    
+    private $formSetPixel;
+
+    public function __construct($acao, $nome, $identifica, $obrigatorio)
+    {
+        parent::__construct($acao, $nome, $identifica, $obrigatorio);
+        $this->formSetPixel = new \Pixel\Form\FormSetPixel();
+    }
+
+    public function setMaximoCaracteres($maximoCaracteres)
+    {
+        parent::setMaximoCaracteres($maximoCaracteres);
+        return $this;
+    }
+
+    public function setMinimoCaracteres($minimoCaracteres)
+    {
+        parent::setMinimoCaracteres($minimoCaracteres);
+        return $this;
+    }
+
+    public function setObrigarorio($obrigatorio)
+    {
+        parent::setObrigarorio($obrigatorio);
+        return $this;
+    }
+
+    public function setPlaceHolder($placeHolder)
+    {
+        parent::setPlaceHolder($placeHolder);
+        return $this;
+    }
+
+    public function setIconFA($iconFA)
+    {
+        $this->iconFA = $this->formSetPixel->setIconFA($iconFA);
+        return $this;
+    }
+
+    public function getIconFA()
+    {
+        return $this->iconFA;
+    }
+    
+    public function setToolTipMsg($toolTipMsg)
+    {
+        $this->toolTipMsg = $this->formSetPixel->setToolTipMsg($toolTipMsg);
+        return $this;
+    }
+
+    public function getToolTipMsg()
+    {
+        return $this->toolTipMsg;
+    }
+    
+    public function setEmColunaDeTamanho($emColunaDeTamanho)
+    {        
+        $this->emColunaDeTamanho = $this->formSetPixel->setEmColunaDeTamanho($emColunaDeTamanho);
+        return $this;
+    }
+
+    public function getemColunaDeTamanho()
+    {
+        return $this->emColunaDeTamanho;
+    }
+
+    /**
+     * Sobrecarga de Metodos Básicos
+     */
+    public function setId($id)
+    {
+        parent::setId($id);
+        return $this;
+    }
+
+    public function setNome($nome)
+    {
+        parent::setNome($nome);
+        return $this;
+    }
+
+    public function setIdentifica($identifica)
+    {
+        parent::setIdentifica($identifica);
+        return $this;
+    }
+
+    public function setValor($valor)
+    {
+        parent::setValor($valor);
+        return $this;
+    }
+
+    public function setValorPadrao($valorPadrao)
+    {
+        parent::setValorPadrao($valorPadrao);
+        return $this;
+    }
+
+    public function setDisabled($disabled)
+    {
+        parent::setDisabled($disabled);
+        return $this;
+    }
+
+    public function setComplemento($complemento)
+    {
+        parent::setComplemento($complemento);
+        return $this;
+    }
+
+    public function setAtributos($atributos)
+    {
+        parent::setAtributos($atributos);
+        return $this;
+    }
+
+    public function setClassCss($classCss)
+    {
+        parent::setClassCss($classCss);
+        return $this;
+    }
+
+}
