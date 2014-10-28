24c72566082af1b4857eb7a7f866ce6b052d8915
Pozzebon <vpozzebon@gmail.com>
2014-10-28 07:39:16 -0300

    Implementada a div sisContainerManu

--- Lib/Pixel/JS/PixelPadrao.js
+++ Lib/Pixel/JS/PixelPadrao.js
@@ -6,4 +6,6 @@
 function sisSpa(p){$("#sisPaginaAtual").val(p);}
 function sisSvo(q,t){$("#sisQuemOrdena").val(q);$("#sisTipoOrdenacao").val(t);}
 function showHiddenFilters(){$(".showHidden").slideToggle();$(".showHidden").removeClass("hidden");}
+function replaceContentElem(e){$(e).fadeToggle('slow', function(){$(e).html('');})}
+function setContentElem(e,c){$(e).html(c);$(e).fadeIn('slow');}
 
