2f1db20e230bb22c9094a008d0e0d52873faf819
Pablo Vanni <pablovanni@gmail.com>
2015-01-05 10:58:57 -0300

    Tradução do menu e documentação do ajax

--- Lib/Zion/JQuery/JQuery.php
+++ Lib/Zion/JQuery/JQuery.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * \Zion\JQuery\JQuery()
- * 
- * @author The Sappiens Team
- * @copyright 2014
- * @version 2014
- * @access public
- */
- 
 namespace Zion\JQuery;
 
 class JQuery
@@ -22,4 +13,5 @@ class JQuery
     {
         return new Ajax();
     }
+
 }
