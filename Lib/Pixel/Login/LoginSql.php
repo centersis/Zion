ca174783c21350b2b868d0af9d4dd52153663eb5
Pozzebon <vpozzebon@gmail.com>
2015-01-09 10:54:31 -0300

    Traduzido o SQL do Login

--- Lib/Pixel/Login/LoginSql.php
+++ Lib/Pixel/Login/LoginSql.php
@@ -7,16 +7,41 @@ class LoginSql
 
     public function getAuth($l, $p)
     {
+
+        $con = \Zion\Banco\Conexao::conectar();
+        $qb = $con->link()->createQueryBuilder();
+
+        $qb->select('*')
+           ->from('_usuario', 'a')
+           ->where('a.usuarioLogin = :usuarioLogin')
+           ->andWhere('a.usuarioSenha = :usuarioSenha')
+           ->setParameters([':usuarioLogin' => $l, ':usuarioSenha' => $p]);    
+
+        return $qb;    
+/*
         return "SELECT usuarioCod, organogramaCod
                   FROM _usuario
                  WHERE usuarioLogin = '" . $l . "' AND usuarioSenha = '" . $p . "' ";
+*/                 
     }
 
     public function getDadosUsuario($cod)
     {
+
+        $con = \Zion\Banco\Conexao::conectar();
+        $qb = $con->link()->createQueryBuilder();
+
+        $qb->select('*')
+           ->from('_usuario', 'a')
+           ->where('a.usuarioCod = :usuarioCod')
+           ->setParameter(':usuarioCod', $cod);    
+
+        return $qb;  
+/*
         return "SELECT *
                   FROM  _usuario
                  WHERE  usuarioCod = ".$cod;
+*/                 
     }    
 
 }
