dd19948c9b31b42fddb88dc290bc0d5f5a877d31
Feliphe <feliphe@braconsultoria.com.br>
2015-01-09 10:40:18 -0200

    Atualização de versão.

--- Lib/Pixel/Login/LoginSql.php
+++ Lib/Pixel/Login/LoginSql.php
@@ -8,8 +8,7 @@ class LoginSql
     public function getAuth($l, $p)
     {
         return "SELECT usuarioCod, organogramaCod
-                  FROM _usuario
-                 WHERE usuarioLogin = '" . $l . "' AND usuarioSenha = '" . $p . "' ";
+                  FROM _usuario";
     }
 
     public function getDadosUsuario($cod)
