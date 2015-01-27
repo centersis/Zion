821ba390443209b108d45c6eba29b1a42cc8b735
Pablo Vanni <pablovanni@gmail.com>
2015-01-27 16:27:17 -0300

    Correção no existe

--- Lib/Zion/Banco/Conexao.php
+++ Lib/Zion/Banco/Conexao.php
@@ -1,32 +1,33 @@
 <?php
+
 /**
-*
-*    Sappiens Framework
-*    Copyright (C) 2014, BRA Consultoria
-*
-*    Website do autor: www.braconsultoria.com.br/sappiens
-*    Email do autor: sappiens@braconsultoria.com.br
-*
-*    Website do projeto, equipe e documentação: www.sappiens.com.br
-*   
-*    Este programa é software livre; você pode redistribuí-lo e/ou
-*    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
-*    publicada pela Free Software Foundation, versão 2.
-*
-*    Este programa é distribuído na expectativa de ser útil, mas SEM
-*    QUALQUER GARANTIA; sem mesmo a garantia implícita de
-*    COMERCIALIZAÇÃO ou de ADEQUAÇÃO A QUALQUER PROPÓSITO EM
-*    PARTICULAR. Consulte a Licença Pública Geral GNU para obter mais
-*    detalhes.
-* 
-*    Você deve ter recebido uma cópia da Licença Pública Geral GNU
-*    junto com este programa; se não, escreva para a Free Software
-*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-*    02111-1307, USA.
-*
-*    Cópias da licença disponíveis em /Sappiens/_doc/licenca
-*
-*/
+ *
+ *    Sappiens Framework
+ *    Copyright (C) 2014, BRA Consultoria
+ *
+ *    Website do autor: www.braconsultoria.com.br/sappiens
+ *    Email do autor: sappiens@braconsultoria.com.br
+ *
+ *    Website do projeto, equipe e documentação: www.sappiens.com.br
+ *   
+ *    Este programa é software livre; você pode redistribuí-lo e/ou
+ *    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+ *    publicada pela Free Software Foundation, versão 2.
+ *
+ *    Este programa é distribuído na expectativa de ser útil, mas SEM
+ *    QUALQUER GARANTIA; sem mesmo a garantia implícita de
+ *    COMERCIALIZAÇÃO ou de ADEQUAÇÃO A QUALQUER PROPÓSITO EM
+ *    PARTICULAR. Consulte a Licença Pública Geral GNU para obter mais
+ *    detalhes.
+ * 
+ *    Você deve ter recebido uma cópia da Licença Pública Geral GNU
+ *    junto com este programa; se não, escreva para a Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *    02111-1307, USA.
+ *
+ *    Cópias da licença disponíveis em /Sappiens/_doc/licenca
+ *
+ */
 
 namespace Zion\Banco;
 
@@ -459,13 +460,20 @@ class Conexao
     {
         $qb = $this->link()->createQueryBuilder();
 
-        $sql = $qb->select($campo)
-                ->from($tabela, '')
-                ->where($qb->expr()->eq($campo, ':campo'))
-                ->setParameter('campo', $inteiro ? $valor : $qb->expr()->literal($valor))
-                ->setMaxResults(1);
+        $qb->select($campo)
+                ->from($tabela, '');
 
-        return($this->execNLinhas($sql) < 1) ? false : true;
+        if ($inteiro) {
+            $qb->where($qb->expr()->eq($campo, $qb->expr()->eq($campo, '?')))
+                    ->setParameter(0, $valor, \PDO::PARAM_INT);
+        } else {
+            $qb->where($qb->expr()->eq($campo, '?'))
+                    ->setParameter(0, $valor, \PDO::PARAM_STR);
+        }
+
+        $qb->setMaxResults(1);
+
+        return ($this->execNLinhas($qb) < 1) ? false : true;
     }
 
     /**
