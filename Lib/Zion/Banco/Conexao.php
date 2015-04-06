a6e133ae5e5485eba10c1b26c9776e387895447a
Pablo Vanni <pablovanni@gmail.com>
2015-04-06 11:26:34 -0400

    Log desabilitado

--- Lib/Zion/Banco/Conexao.php
+++ Lib/Zion/Banco/Conexao.php
@@ -194,7 +194,7 @@ class Conexao
             }
 
             if ($sql->getType() !== 0 and $this->linhasAfetadas > 0) {
-                (new Log())->registraLog($sql, self::$logHash);
+                //(new Log())->registraLog($sql, self::$logHash);
             }
 
             return $resultSet;
