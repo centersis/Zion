9d1f470b82928f88d409790e3fe970fd60d9ac67
Bueno, Feliphe <feliphe@braconsultoria.com.br>
2015-04-23 15:23:48 -0100

    Correção na validação de campos do tipo float.

--- Lib/Zion/Validacao/Numero.php
+++ Lib/Zion/Validacao/Numero.php
@@ -109,7 +109,7 @@ class Numero extends \Zion\Tratamento\Numero
      */
     public function isFloat($numero)
     {
-        if (preg_match('/[0-9]{1,3}[\.|,][0-9]{1,2}$/', $numero) and is_numeric($numero)) {
+        if (\preg_match('/[0-9]{1,3}[\.|,][0-9]{1,2}$/', $numero)) {
             return true;
         } else {
             return false;
