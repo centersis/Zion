b3126709f8f9a28e007c4a0c9f8a0c889607cb3a
Vinicius Pozzebon <vpozzebon@gmail.com>
2015-07-07 11:28:38 -0400

    Componente para ordenar Array

--- Lib/Zion/Tratamento/Geral.php
+++ Lib/Zion/Tratamento/Geral.php
@@ -188,5 +188,48 @@ class Geral
     {
         throw new RuntimeException("Metoto ainda nao implementado.");
     }
+    
+    /**
+     * Geral::ordenaArray()
+     * 
+     * @param array $array
+     * @param mixed $on
+     * @param mixed $order
+     * @return
+     */    
+    public function ordenaArray($array, $on, $order=SORT_ASC)
+    {
+        $new_array = array();
+        $sortable_array = array();
+
+        if (\count($array) > 0) {
+            foreach ($array as $k => $v) {
+                if (\is_array($v)) {
+                    foreach ($v as $k2 => $v2) {
+                        if ($k2 == $on) {
+                            $sortable_array[$k] = $v2;
+                        }
+                    }
+                } else {
+                    $sortable_array[$k] = $v;
+                }
+            }
+
+            switch ($order) {
+                case \SORT_ASC:
+                    \asort($sortable_array);
+                break;
+                case \SORT_DESC:
+                    \arsort($sortable_array);
+                break;
+            }
+
+            foreach ($sortable_array as $k => $v) {
+                $new_array[$k] = $array[$k];
+            }
+        }
+
+        return $new_array;
+    }      
 
 }
