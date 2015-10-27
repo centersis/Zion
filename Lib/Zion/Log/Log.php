d6c0d698d050819ae2fcb1c64028dcf811bb062c
Pablo Vanni <pablovanni@gmail.com>
2015-10-27 11:10:51 -0300

    correções no log

--- Lib/Zion/Log/Log.php
+++ Lib/Zion/Log/Log.php
@@ -1,159 +1,174 @@
 <?php
+
 /**
-*
-*    Sappiens Framework
-*    Copyright (C) 2014, BRA Consultoria
-*
-*    Website do autor: www.braconsultoria.com.br/sappiens
-*    Email do autor: sappiens@braconsultoria.com.br
-*
-*    Website do projeto, equipe e documentação: www.sappiens.com.br
-*   
-*    Este programa é software livre; você pode redistribuí-lo e/ou
-*    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
-*    publicada pela Free Software Foundation, versão 2.
-*
-*    Este programa é distribuído na expectativa de ser útil, mas SEM
-*    QUALQUER GARANTIA; sem mesmo a garantia implícita de
-*    COMERCIALIZAÇÃO ou de ADEQUAÇÃO A QUALQUER PROPÓSITO EM
-*    PARTICULAR. Consulte a Licença Pública Geral GNU para obter mais
-*    detalhes.
-* 
-*    Você deve ter recebido uma cópia da Licença Pública Geral GNU
-*    junto com este programa; se não, escreva para a Free Software
-*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-*    02111-1307, USA.
-*
-*    Cópias da licença disponíveis em /Sappiens/_doc/licenca
-*
-*/
+ *
+ *    Sappiens Framework
+ *    Copyright (C) 2014, BRA Consultoria
+ *
+ *    Website do autor: www.braconsultoria.com.br/sappiens
+ *    Email do autor: sappiens@braconsultoria.com.br
+ *
+ *    Website do projeto, equipe e documentação: www.sappiens.com.br
+ *   
+ *    Este programa é software livre; você pode redistribuí-lo e/ou
+ *    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+ *    publicada pela Free Software Foundation, versão 2.
+ *
+ *    Este programa é distribuído na expectativa de ser útil, mas SEM
+ *    QUALQUER GARANTIA; sem mesmo a garantia implícita de
+ *    COMERCIALIZAÇÃO ou de ADEQUAÇÃO A QUALQUER PROPÓSITO EM
+ *    PARTICULAR. Consulte a Licença Pública Geral GNU para obter mais
+ *    detalhes.
+ * 
+ *    Você deve ter recebido uma cópia da Licença Pública Geral GNU
+ *    junto com este programa; se não, escreva para a Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *    02111-1307, USA.
+ *
+ *    Cópias da licença disponíveis em /Sappiens/_doc/licenca
+ *
+ */
 
 namespace Zion\Log;
 
 class Log extends LogSql
 {
-    protected $con;
-    
-    public function __construct() 
-    {
-        $this->con = \Zion\Banco\Conexao::conectar();       
-    }
-    
+
     /**
-     * Registro de LOG segundo a perspectiva humana
      * 
-     * 
+     * @param int $usuarioCod
+     * @param string $moduloNome
+     * @param string $logAcao
+     * @param \Doctrine\DBAL\Query\QueryBuilder $logSql
+     * @param int $logId
+     * @param string $logHash
+     * @param string $logTab
      */
-    public function registraLogUsuario($usuarioCod, $moduloNome, $logAcao, \Doctrine\DBAL\Query\QueryBuilder $logSql, $logId = NULL, $logHash = false, $logTab = NULL)
+    public function registraLogUsuario($usuarioCod, $moduloNome, $logAcao, $logSql, $logId = null, $logHash = false, $logTab = null)
     {
         $this->salvarlLog(['usuarioCod' => $usuarioCod,
-                            'moduloCod' => $this->getDadosModulo($moduloNome)['modulocod'], 
-                            'id'        => $logId, 
-                            'acao'      => $logAcao,
-                            'tab'       => $logTab
-                           ], $this->getSqlCompleta($logSql), ($logHash ?: \bin2hex(\openssl_random_pseudo_bytes(25))));
+            'moduloCod' => $this->getDadosModulo($moduloNome)['modulocod'],
+            'id' => $logId,
+            'acao' => $logAcao,
+            'tab' => $logTab
+            ], $this->getSqlCompleta($logSql), ($logHash ? : \bin2hex(\openssl_random_pseudo_bytes(6))));
     }
-    
-    public function registraLog(\Doctrine\DBAL\Query\QueryBuilder $sql, $logHash)
+
+    /**
+     * 
+     * @param \Doctrine\DBAL\Query\QueryBuilder $sql
+     * @param string $logHash
+     */
+    public function registraLog($sql, $logHash)
     {
-        $sqlCompleta    = $this->getSqlCompleta($sql);
-        $actParams      = $this->getActionParams();
+        $sqlCompleta = $this->getSqlCompleta($sql);
+        $actParams = $this->getActionParams();
 
-        if(empty($actParams['id'])){
+        if (empty($actParams['id'])) {
 
-            if($sql->getType() === 1 or $sql->getType() === 2){
+            if ($sql->getType() === 1 or $sql->getType() === 2) {
                 $actParams['id'] = $this->getIdRegistroSql($sql, $sqlCompleta);
-            } elseif($sql->getType() === 3) {
-                $actParams['id'] = NULL;
+            } elseif ($sql->getType() === 3) {
+                $actParams['id'] = null;
             }
         }
-        
+
         $this->salvarlLog($actParams, $sqlCompleta, $logHash);
     }
-    
+
     private function getActionParams()
     {
-        $modulo = $this->getDadosModulo(MODULO);
-        $id     = \filter_input(\INPUT_POST, 'cod');
-        $tab    = \filter_input(\INPUT_POST, 'n');
-        $acao   = \filter_input(\INPUT_GET, 'acao');
-              
-        return ['usuarioCod'    => $_SESSION['usuarioCod'],
-                'moduloCod'     => $modulo['modulocod'], 
-                'id'            => $id, 
-                'acao'          => $acao,
-                'tab'           => $tab
-               ];
+        $modulo = $this->getDadosModulo(\MODULO);
+        $id = \filter_input(\INPUT_POST, 'cod');
+        $tab = \filter_input(\INPUT_POST, 'n');
+        $acao = \filter_input(\INPUT_GET, 'acao');
 
+        return ['usuarioCod' => $_SESSION['usuarioCod'],
+            'moduloCod' => $modulo['modulocod'],
+            'id' => $id,
+            'acao' => $acao,
+            'tab' => $tab
+        ];
     }
-    
-    private function getIdRegistroSql(\Doctrine\DBAL\Query\QueryBuilder $sql, $sqlCompleta)
+
+    /**
+     * 
+     * @param \Doctrine\DBAL\Query\QueryBuilder $sql
+     * @param string $sqlCompleta
+     * @return type
+     */
+    private function getIdRegistroSql($sql, $sqlCompleta)
     {
         $parts = $this->getAtributosPrivados($sql->getQueryPart('where'));
-        
-        if(!isset($parts['parts'])){
-            return NULL;
-        }
-        
-        $idRegistro = NULL;
 
-        foreach($parts['parts'] as $k => $v) {
-            
+        if (!isset($parts['parts'])) {
+            return null;
+        }
+
+        $idRegistro = null;
+
+        foreach ($parts['parts'] as $k => $v) {
+
             $param = \preg_replace('/\s/', '', \explode('=', $v)[0]);
             $matches = [];
-            \preg_match('/'. $param .'\s=\s[0-9]{1,}/', $sqlCompleta, $matches);
-            
+            \preg_match('/' . $param . '\s=\s[0-9]{1,}/', $sqlCompleta, $matches);
+
             $valParam = (int) \explode('=', $matches[0])[1];
-            
-            if($valParam > 0){
+
+            if ($valParam > 0) {
                 $idRegistro = $valParam;
                 break;
             }
         }
-        
+
         return $idRegistro;
     }
-    
+
     private function getAtributosPrivados($input)
     {
         $attrs = array();
         foreach (((array) $input) as $key => $val) {
 
-            $key = preg_replace(array('/'. \addslashes('Doctrine\DBAL\Query\Expression\CompositeExpression') .'/', '/\W/'), '', $key);
+            $key = \ preg_replace(array('/' . \addslashes('Doctrine\DBAL\Query\Expression\CompositeExpression') . '/', '/\W/'), '', $key);
             $attrs[$key] = $val;
         }
 
         return $attrs;
     }
 
-    private function getSqlCompleta(\Doctrine\DBAL\Query\QueryBuilder $sql)
+    /**
+     * 
+     * @param \Doctrine\DBAL\Query\QueryBuilder $sql
+     * @return type
+     */
+    private function getSqlCompleta($sql)
     {
-        $params     = $sql->getParameters();
+        $params = $sql->getParameters();
 
         $paramTypes = \array_map(function($param) {
-                        return (\is_numeric($param) ? 1 : 2);
-                      }, $params);
+            return (\is_numeric($param) ? 1 : 2);
+        }, $params);
 
-        
+
         $sqlCompleta = $sql->getSQL();
-        $i = 0;
 
-        foreach($paramTypes as $param => $type){
+        foreach ($paramTypes as $param => $type) {
 
-            $replacement = ($type == 1 ? $params[$param] : "'". $params[$param] ."'");
-            $sqlCompleta = preg_replace(['/:'. $param .'/', '/\?/'], $replacement, $sqlCompleta, 1);
+            $replacement = ($type == 1 ? $params[$param] : "'" . $params[$param] . "'");
+            $sqlCompleta = \preg_replace(['/:' . $param . '/', '/\?/'], $replacement, $sqlCompleta, 1);
         }
-        
+
         return $sqlCompleta;
     }
-    
+
     protected function getDadosModulo($moduloNome)
     {
         return $this->con->execLinha(parent::getDadosModuloSql($moduloNome));
     }
-    
+
     protected function salvarlLog($actParams, $sqlCompleta, $logHash)
     {
         parent::salvarLogSql($actParams, $sqlCompleta, $logHash)->execute();
     }
+
 }
--- Lib/Zion/Log/LogSql.php
+++ Lib/Zion/Log/LogSql.php
@@ -1,80 +1,100 @@
 <?php
+
 /**
-*
-*    Sappiens Framework
-*    Copyright (C) 2014, BRA Consultoria
-*
-*    Website do autor: www.braconsultoria.com.br/sappiens
-*    Email do autor: sappiens@braconsultoria.com.br
-*
-*    Website do projeto, equipe e documentação: www.sappiens.com.br
-*   
-*    Este programa é software livre; você pode redistribuí-lo e/ou
-*    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
-*    publicada pela Free Software Foundation, versão 2.
-*
-*    Este programa é distribuído na expectativa de ser útil, mas SEM
-*    QUALQUER GARANTIA; sem mesmo a garantia implícita de
-*    COMERCIALIZAÇÃO ou de ADEQUAÇÃO A QUALQUER PROPÓSITO EM
-*    PARTICULAR. Consulte a Licença Pública Geral GNU para obter mais
-*    detalhes.
-* 
-*    Você deve ter recebido uma cópia da Licença Pública Geral GNU
-*    junto com este programa; se não, escreva para a Free Software
-*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-*    02111-1307, USA.
-*
-*    Cópias da licença disponíveis em /Sappiens/_doc/licenca
-*
-*/
+ *
+ *    Sappiens Framework
+ *    Copyright (C) 2014, BRA Consultoria
+ *
+ *    Website do autor: www.braconsultoria.com.br/sappiens
+ *    Email do autor: sappiens@braconsultoria.com.br
+ *
+ *    Website do projeto, equipe e documentação: www.sappiens.com.br
+ *   
+ *    Este programa é software livre; você pode redistribuí-lo e/ou
+ *    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+ *    publicada pela Free Software Foundation, versão 2.
+ *
+ *    Este programa é distribuído na expectativa de ser útil, mas SEM
+ *    QUALQUER GARANTIA; sem mesmo a garantia implícita de
+ *    COMERCIALIZAÇÃO ou de ADEQUAÇÃO A QUALQUER PROPÓSITO EM
+ *    PARTICULAR. Consulte a Licença Pública Geral GNU para obter mais
+ *    detalhes.
+ * 
+ *    Você deve ter recebido uma cópia da Licença Pública Geral GNU
+ *    junto com este programa; se não, escreva para a Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *    02111-1307, USA.
+ *
+ *    Cópias da licença disponíveis em /Sappiens/_doc/licenca
+ *
+ */
 
 namespace Zion\Log;
 
+use Zion\Banco\Conexao;
+
 class LogSql
 {
+
+    protected $con;
+
+    public function __construct()
+    {
+        $idBanco = null;
+
+        $namespace = '\\' . \SIS_ID_NAMESPACE_PROJETO . '\\Config';
+
+        $cBanco = $namespace::$SIS_CFG['bases']['padrao']['banco'];
+
+        if (\array_key_exists('log', $namespace::$SIS_CFG['bases']) and $namespace::$SIS_CFG['bases']['log']['banco'] <> $cBanco) {
+            $idBanco = 'log';
+        }
+
+        $this->con = Conexao::conectar($idBanco);
+    }
+
     protected function getDadosModuloSql($moduloNome)
     {
         $qb = $this->con->qb();
 
         $qb->select('*')
-                ->from('_modulo', 'a')
-                ->where('a.moduloNome = :moduloNome')
-                ->setParameter('moduloNome', $moduloNome);
+            ->from('_modulo', 'a')
+            ->where('a.moduloNome = :moduloNome')
+            ->setParameter('moduloNome', $moduloNome);
 
         return $qb;
     }
-    
+
     protected function salvarLogSql($actParams, $sqlCompleta, $logHash)
     {
         /* @var $qb \Doctrine\DBAL\Query\QueryBuilder */
         $qb = $this->con->qb();
 
         $qb->insert('_log')
-           ->values(['usuarioCod'   => ':usuarioCod',
-                     'moduloCod'    => ':moduloCod',
-                     'logHash'      => ':logHash',
-                     'logId'        => ':logId',
-                     'logAcao'      => ':logAcao',
-                     'logTab'       => ':logTab',
-                     'logSql'       => ':logSql',
-                     'logDataHora'  => $qb->expr()->literal(date('Y-m-d H:i:s'))
-                    ])
-           ->setParameters(['usuarioCod'   => $actParams['usuarioCod'],
-                            'moduloCod'    => $actParams['moduloCod'],
-                            'logHash'      => $logHash,
-                            'logId'        => $actParams['id'],
-                            'logAcao'      => $actParams['acao'],
-                            'logTab'       => $actParams['tab'],
-                            'logSql'       => $sqlCompleta
-                           ],
-                           ['usuarioCod'   => \PDO::PARAM_INT,
-                            'moduloCod'    => \PDO::PARAM_INT,
-                            'logHash'      => \PDO::PARAM_STR,
-                            'logId'        => \PDO::PARAM_INT,
-                            'logAcao'      => \PDO::PARAM_STR,
-                            'logTab'       => \PDO::PARAM_STR,
-                            'logSql'       => \PDO::PARAM_STR
-                           ]);
+            ->values(['usuarioCod' => ':usuarioCod',
+                'moduloCod' => ':moduloCod',
+                'logHash' => ':logHash',
+                'logId' => ':logId',
+                'logAcao' => ':logAcao',
+                'logTab' => ':logTab',
+                'logSql' => ':logSql',
+                'logDataHora' => $qb->expr()->literal(date('Y-m-d H:i:s'))
+            ])
+            ->setParameters(['usuarioCod' => $actParams['usuarioCod'],
+                'moduloCod' => $actParams['moduloCod'],
+                'logHash' => $logHash,
+                'logId' => $actParams['id'],
+                'logAcao' => $actParams['acao'],
+                'logTab' => $actParams['tab'],
+                'logSql' => $sqlCompleta
+                ], ['usuarioCod' => \PDO::PARAM_INT,
+                'moduloCod' => \PDO::PARAM_INT,
+                'logHash' => \PDO::PARAM_STR,
+                'logId' => \PDO::PARAM_INT,
+                'logAcao' => \PDO::PARAM_STR,
+                'logTab' => \PDO::PARAM_STR,
+                'logSql' => \PDO::PARAM_STR
+        ]);
 
         return $qb;
     }
