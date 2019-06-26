a40dd2d1e5a64acfde2b846d419d858caf6daaaa
Pablo Vanni <pablovanni@gmail.com>
2019-06-26 09:45:21 -0400

    Commit do Julio ajustando crop

--- Lib/Pixel/Form/FormPixelJavaScript.php
+++ Lib/Pixel/Form/FormPixelJavaScript.php
@@ -309,13 +309,12 @@ class FormPixelJavaScript
                 'previewLargura' => '100%',
                 'previewAltura' => '100',
                 'arquivoLargura' => '100',
-                'arquivoAltura' => '100',
+                'arquivoAltura' => '100'
             ];
 
             $cCrop = [];
 
             foreach ($defaultCrop as $chave => $valor) {
-
                 $cCrop[$chave] = $configCrop[$chave] ?? $valor;
             }
 
@@ -331,9 +330,49 @@ class FormPixelJavaScript
                 $txtSize = 'minSize:[' . $cCrop['cropLarguraMinima'] . ',' . $cCrop['cropAlturaMinima'] . '], maxSize:[' . $cCrop['cropLarguraMaxima'] . ',' . $cCrop['cropAlturaMaxima'] . ']';
             }
 
-            //$this->extra[] = 'imagemQS=document.querySelector("#' . $formNome . ' #sis_demo_crop_' . $config->getId() . '"); if(imagemQS.naturalWidth < '.$cCrop['imagemLarguraMinima'].' || imagemQS.naturalHeight < '.$cCrop['imagemAlturaMinima'].') { sisSetAlert("false", "A imagem deve ter dimensões mínimas de '.$cCrop['imagemAlturaMinima'].' pixels de altura por '.$cCrop['imagemLarguraMinima'].' pixels de largura!"); };';
-
-            $this->extra[] = '$("#' . $formNome . ' #' . $config->getId() . '").change(function(e){$imagem=$("#' . $formNome . ' #sis_demo_crop_' . $config->getId() . '");$imagem.show();var a=new FileReader;a.onload=function(e){var a=new Image;a.onload=function(){var e=a.width,t=a.height;e>t?e>500&&(t*=500/e,e=500):t>500&&(e*=500/t,t=500);var r=document.createElement("canvas");r.width=e,r.height=t,r.getContext("2d").drawImage(this,0,0,e,t),this.src=r.toDataURL()},a.src=e.target.result,$("#' . $formNome . ' #sis_demo_crop_' . $config->getId() . '").attr("src",e.target.result),$imagem.rcrop({' . $txtSize . ', preserveAspectRatio:!1,preview:{display:!0,size:["' . $txtPreview . '],wrapper:""}}),$imagem.on("rcrop-changed rcrop-ready",function(){var e=$(this).rcrop("getDataURL",' . $txtArquivo . ');$("#' . $formNome . ' #sis_base64_crop_' . $config->getId() . '").val(e)})},a.readAsDataURL(e.target.files[0]),$imagem.rcrop("destroy"),$("canvas").remove()});';
+            $this->extra[] = '
+            $("#' . $formNome . ' #' . $config->getId() . '").change(
+                function(html){
+                    $imagem=$("#' . $formNome . ' #sis_demo_crop_' . $config->getId() . '");
+                    $imagem.show();
+                    var arquivo=new FileReader;
+                    arquivo.onload=
+                        function(file){
+                            var imageFile=new Image;
+                            imageFile.onload=
+                                function(){
+                                    var e=imageFile.width,t=imageFile.height;
+                                    e>t?e>500&&(t*=500/e,e=500):t>500&&(e*=500/t,t=500);
+                                    var r=document.createElement("canvas");
+                                    r.width=e;
+                                    r.height=t;
+                                    r.getContext("2d").drawImage(this,0,0,e,t);
+                                    this.src=r.toDataURL();
+                                    this.onload = function() {
+                                        if(imageFile.width < ' . $cCrop['imagemLarguraMinima'] . ' || imageFile.height < ' . $cCrop['imagemAlturaMinima'] . ') {
+                                            $("#' . $formNome . ' #sis_demo_crop_' . $config->getId() . '").removeAttr("src");
+                                            $("#' . $formNome . ' #sis_base64_crop_' . $config->getId() . '").removeAttr("value");
+                                            $("#' . $formNome . ' #' . $config->getId() . '").val(null);
+                                            sisSetAlert("false", "A imagem deve ter dimensões mínimas de ' . $cCrop['imagemAlturaMinima'] . ' pixels de altura por ' . $cCrop['imagemLarguraMinima'] . ' pixels de largura!");
+                                        } else {
+                                            $("#' . $formNome . ' #sis_demo_crop_' . $config->getId() . '").attr("src",file.target.result);
+                                            $imagem.rcrop({' . $txtSize . ', preserveAspectRatio:!1,preview:{display:!0,size:["' . $txtPreview . '],wrapper:""}});
+                                            $imagem.on("rcrop-changed rcrop-ready",
+                                                function(){
+                                                    var imagemCortada=$(this).rcrop("getDataURL",' . $txtArquivo . ');
+                                                    $("#' . $formNome . ' #sis_base64_crop_' . $config->getId() . '").val(imagemCortada)
+                                                }
+                                            )
+                                        }
+                                    }
+                                };
+                            imageFile.src=file.target.result;
+                        };
+                    arquivo.readAsDataURL(html.target.files[0]);
+                    $imagem.rcrop("destroy");
+                    $("canvas").remove();
+                }
+            );';
         }
     }
 
