7ea1b5406f16547bc6ae13636c9ecefc54ff8723
Pablo Vanni <pablovanni@gmail.com>
2023-11-22 17:32:36 -0400

    atuliza csv e pdf

--- Lib/Zion/Exportacao/CSV.php
+++ Lib/Zion/Exportacao/CSV.php
@@ -4,6 +4,7 @@ namespace Zion\Exportacao;
 
 class CSV
 {
+
     /**
      * CSV::getCSV()
      * 
@@ -13,69 +14,73 @@ class CSV
     {
         return self::geraRelatorio();
     }
-    
+
     /**
      * CSV::geraRelatorio()
      * 
      * @return
      */
-    private function geraRelatorio(){
+    private function geraRelatorio()
+    {
 
         $dadosRelatorio = parent::getDadosRelatorio();
         $colunas = parent::getColunas();
-        
+
         $numColunas = count($colunas);
 
-        if(!is_array($dadosRelatorio)) return false;
-        if($numColunas < 1) return false;
+        if (!is_array($dadosRelatorio))
+            return false;
+        if ($numColunas < 1)
+            return false;
 
         $dadosCSV = NULL;
-        foreach($colunas as $col=>$name){
-            $dadosCSV .= ($name) .';';
+        foreach ($colunas as $col => $name) {
+            $dadosCSV .= ($name) . ';';
         }
 
-        $dadosCSV =  substr($dadosCSV, false, -1) . "\n";
+        $dadosCSV = substr($dadosCSV, false, -1) . "\n";
 
         $i = 0;
 
-        while($i <= count($dadosRelatorio)){
+        while ($i <= count($dadosRelatorio)) {
 
-            if(!@is_array($dadosRelatorio[$i])) {
+            if (!@is_array($dadosRelatorio[$i])) {
                 $i++;
                 continue;
             }
-            
+
             $linha = NULL;
 
-            foreach($colunas as $col=>$name){
-                $linha .= ($dadosRelatorio[$i][$col]) .';';
+            foreach ($colunas as $col => $name) {
+                $linha .= ($dadosRelatorio[$i][$col]) . ';';
             }
-            
+
             $dadosCSV .= substr($linha, false, -1) . "\n";
 
             $i++;
         }
 
-        $nome       =  uniqid() .'.csv';
-        $caminho    = SIS_DIR_BASE . 'arquivos/'. $nome;
+        $nome = uniqid() . '.csv';
+        $caminho = SIS_DIR_BASE . 'arquivos/' . $nome;
 
-		$arquivo = fopen($caminho, "a");
+        $arquivo = fopen($caminho, "a");
 
-		@fwrite($arquivo, $dadosCSV);
+        @fwrite($arquivo, $dadosCSV);
 
-		if (!file_exists($caminho)) return false;
-		header('Pragma: public');
-		header('Expires: 0');
-		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-		header('Cache-Control: private', false);
-		header('Content-Type: application/octet-stream');
-		header('Content-Disposition: attachment; filename='. $nome .';');
-		header('Content-Transfer-Encoding: binary');
-		header('Content-Length: '. @filesize($caminho));
-		set_time_limit(0);
-		if(!@readfile($caminho)) return false;
-		return(@fclose($arquivo)) ? true : false;
-
+        if (!file_exists($caminho))
+            return false;
+        header('Pragma: public');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Cache-Control: private', false);
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . $nome . ';');
+        header('Content-Transfer-Encoding: binary');
+        header('Content-Length: ' . @filesize($caminho));
+        set_time_limit(0);
+        if (!@readfile($caminho))
+            return false;
+        return(@fclose($arquivo)) ? true : false;
     }
-  
+
 }
--- Lib/Zion/Exportacao/PDF.php
+++ Lib/Zion/Exportacao/PDF.php
@@ -2,12 +2,12 @@
 
 namespace Zion\Exportacao;
 
-class PDF 
+class PDF
 {
-    
+
     private $con;
-    
-    public function __construct() 
+
+    public function __construct()
     {
         $this->con = \Zion\Banco\Conexao::conectar();
     }
@@ -15,91 +15,90 @@ class PDF
     public function impressaoGridPDF($dados, $cssFile, $cssPath, $controller, $logo, $orientacao = "P")
     {
         $texto = \Zion\Tratamento\Texto::instancia();
-        
+
         $nomeModulo = (new \Base\Sistema\Modulo\ModuloClass())->getDadosModulo(MODULO)['modulonomemenu'];
-        
-        $tituloRelatorio    = (\filter_input(\INPUT_GET, 'sisUFC') ? $this->getUsuarioFiltroNome(\filter_input(\INPUT_GET, 'sisUFC')) : "Relatório do módulo ". $nomeModulo);
-        $nomeArquivo        = \preg_replace('/[^A-z]/', '-', \strtolower($texto->removerAcentos($tituloRelatorio))) . '_'. \date('d-m-Y_H-i-s') .'.pdf';
+
+        $tituloRelatorio = (\filter_input(\INPUT_GET, 'sisUFC') ? $this->getUsuarioFiltroNome(\filter_input(\INPUT_GET, 'sisUFC')) : "Relatório do módulo " . $nomeModulo);
+        $nomeArquivo = \preg_replace('/[^A-z]/', '-', \strtolower($texto->removerAcentos($tituloRelatorio))) . '_' . \date('d-m-Y_H-i-s') . '.pdf';
 
         try {
 
-            if(\count($dados) < 1){
+            if (\count($dados) < 1) {
                 throw new \Exception('Nenhum dado a ser exibido!');
             }
-            
-            $pdfPath = \SIS_DIR_BASE .'Storage/PDF/';
+
+            $pdfPath = \SIS_DIR_BASE . 'Storage/PDF/';
 
             include_once(SIS_FM_BASE . 'Lib/mPDF/mpdf.php');
-            
+
             $stylesheet = $this->loadCss($cssFile, $cssPath);
-            
+
             $dadosHtml = \json_decode($dados['retorno'], true);
 
             $html = $controller->layout()->render('impressao_grid_pdf.html.twig', [
-                                                    'grid'              => ['retorno' => $dadosHtml],
-                                                    'logo'              => $logo,
-                                                    'modulo'            => $nomeModulo,
-                                                    'titulo'            => $tituloRelatorio,
-                                                    'dataRelatorio'     => date("d/m/Y \à\s H:i:s")
-                                                   ]);
+                'grid' => ['retorno' => $dadosHtml],
+                'logo' => $logo,
+                'modulo' => $nomeModulo,
+                'titulo' => $tituloRelatorio,
+                'dataRelatorio' => date("d/m/Y \à\s H:i:s")
+            ]);
 
-            $mpdf = new \mPDF('c', 'A4-'. \strtoupper($orientacao));
+            $mpdf = new \mPDF('c', 'A4-' . \strtoupper($orientacao));
 
             $mpdf->CurOrientation = "L";
 
             $mpdf->allow_charset_conversion = true;
-            $mpdf->charset_in    = 'UTF-8';
-            
-            if(\strlen($dadosHtml['legenda']) > 5){
+            $mpdf->charset_in = 'UTF-8';
+
+            if (\strlen($dadosHtml['legenda']) > 5) {
                 $mpdf->SetHTMLFooter($dadosHtml['legenda']);
             } else {
                 $mpdf->SetFooter('{PAGENO}/{nbpg}');
             }
-            
+
             $mpdf->WriteHTML($stylesheet, 1);
             $mpdf->WriteHTML($html);
 
-            $mpdf->Output($pdfPath . uniqid() .'_relatorio_'. \strtolower(MODULO) .'_'. date('d-m-Y') .'.pdf', 'F');
+            $mpdf->Output($pdfPath . uniqid() . '_relatorio_' . \strtolower(MODULO) . '_' . date('d-m-Y') . '.pdf', 'F');
             $mpdf->Output($nomeArquivo, 'D');
-
-         } catch(\Exception $e) {
-             throw new \Zion\Exportacao\Exception($e->getMessage());
-         }
+        } catch (\Exception $e) {
+            throw new \Zion\Exportacao\Exception($e->getMessage());
+        }
     }
-    
+
     public function getUsuarioFiltroNome($sisUFC)
     {
-        if(!empty($sisUFC)){
+        if (!empty($sisUFC)) {
             $qb = $this->con->qb();
-            
+
             $qb->select('*')
-               ->from('_usuario_filtro')
-               ->where($qb->expr()->eq('usuarioFiltroCod', ':usuarioFiltroCod'))
-               ->setParameter('usuarioFiltroCod', $sisUFC, \PDO::PARAM_INT)
-               ->setMaxResults(1);
-            
+                ->from('_usuario_filtro')
+                ->where($qb->expr()->eq('usuarioFiltroCod', ':usuarioFiltroCod'))
+                ->setParameter('usuarioFiltroCod', $sisUFC, \PDO::PARAM_INT)
+                ->setMaxResults(1);
+
             $linha = $this->con->execLinha($qb);
-            
-            if(isset($linha['usuariofiltronomerelatorio'])){
+
+            if (isset($linha['usuariofiltronomerelatorio'])) {
                 return $linha['usuariofiltronomerelatorio'];
             } else {
                 return false;
             }
         }
     }
-    
-    private function loadCss($cssFile, $cssPath = false) 
+
+    private function loadCss($cssFile, $cssPath = false)
     {
-        if($cssPath === false){
+        if ($cssPath === false) {
             $cssPath = \SIS_URL_DEFAULT_BASE . 'Tema/Vendor/Pixel/1.3.0/stylesheets/';
         }
-        
+
         $files = \preg_replace('/\\n/', '', \file_get_contents($cssPath . $cssFile));
-        
+
         $css = NULL;
         $matches = [];
-        
-        if(\preg_match_all('/[\@import\surl(\']{13}/', $files, $matches, \PREG_OFFSET_CAPTURE)){
+
+        if (\preg_match_all('/[\@import\surl(\']{13}/', $files, $matches, \PREG_OFFSET_CAPTURE)) {
 
             foreach ($matches[0] as $val) {
 
@@ -112,9 +111,9 @@ class PDF
                 if (isset($end[0][1]) === false) {
                     continue;
                 }
-                
+
                 $length = ($end[0][1] - $start);
-                
+
                 $file = \substr($files, ($start), ($length));
 
                 if (!preg_match('/[http\:\/\/]{7}|[https\:\/\/]{8}/', $file)) {
@@ -134,68 +133,67 @@ class PDF
     {
         $texto = \Zion\Tratamento\Texto::instancia();
 
-        $nomeArquivo    = \preg_replace('/[^A-z|^0-9]/', '-', \strtolower($texto->removerAcentos($tituloRelatorio))) . '_'. \date('d-m-Y_H-i-s') .'.pdf';
-        
+        $nomeArquivo = \preg_replace('/[^A-z|^0-9]/', '-', \strtolower($texto->removerAcentos($tituloRelatorio))) . '_' . \date('d-m-Y_H-i-s') . '.pdf';
+
         try {
 
-            if(\count($html) < 1){
+            if (\count($html) < 1) {
                 throw new \Exception('Nenhum dado a ser exibido!');
             }
-            
-            $pdfPath = \SIS_DIR_BASE .'Storage/PDF/';
-            
-            if(!\is_readable($pdfPath)){
+
+            $pdfPath = \SIS_DIR_BASE . 'Storage/PDF/';
+
+            if (!\is_readable($pdfPath)) {
                 \mkdir($pdfPath, 0777);
             }
 
             include_once(SIS_FM_BASE . 'Lib/mPDF/mpdf.php');
-            
-            if($cssPath !== NULL and $cssFile !== NULL){
+
+            if ($cssPath !== NULL and $cssFile !== NULL) {
                 $stylesheet = $this->loadCss($cssFile, $cssPath);
             }
 
-            $mpdf = new \mPDF('c', 'A4-'. \strtoupper($orientacao));
+            $mpdf = new \mPDF('c', 'A4-' . \strtoupper($orientacao));
 
             $mpdf->allow_charset_conversion = true;
-            $mpdf->charset_in    = 'UTF-8';
-            
-            if(\strlen($legenda) > 5){
+            $mpdf->charset_in = 'UTF-8';
+
+            if (\strlen($legenda) > 5) {
                 $mpdf->SetHTMLFooter($legenda);
             } else {
                 $mpdf->SetFooter('{PAGENO}/{nbpg}');
             }
-            
+
             $mpdf->WriteHTML($stylesheet, 1);
             $mpdf->WriteHTML($html);
             $mpdf->Output($nomeArquivo, $output);
-            
-            return false;
 
-         } catch(\Exception $e) {
+            return false;
+        } catch (\Exception $e) {
             return $e->getMessage();
-         }
+        }
     }
-    
+
     public function salvaArquivoPDF($html, $cssFile = NULL, $cssPath = NULL, $filePathName = NULL, $legenda = false, $orientacao = "P")
-    {        
+    {
         try {
 
-            if(\count($html) < 1){
+            if (\count($html) < 1) {
                 throw new \Exception('Nenhum dado a ser exibido!');
             }
 
             include_once(\SIS_FM_BASE . 'Lib/mPDF/mpdf.php');
-            
-            if($cssPath !== NULL and $cssFile !== NULL){
+
+            if ($cssPath !== NULL and $cssFile !== NULL) {
                 $stylesheet = $this->loadCss($cssFile, $cssPath);
             }
 
-            $mpdf = new \mPDF('c', 'A4-'. \strtoupper($orientacao));
+            $mpdf = new \mPDF('c', 'A4-' . \strtoupper($orientacao));
 
             $mpdf->allow_charset_conversion = true;
-            $mpdf->charset_in    = 'UTF-8';
+            $mpdf->charset_in = 'UTF-8';
 
-            if($legenda !== false){
+            if ($legenda !== false) {
                 $mpdf->SetHTMLFooter($legenda);
             } else {
                 $mpdf->SetFooter('{PAGENO}/{nbpg}');
@@ -206,9 +204,9 @@ class PDF
             $mpdf->Output($filePathName, 'F');
 
             return false;
-
-         } catch(\Exception $e) {
+        } catch (\Exception $e) {
             return $e->getMessage();
-         }
+        }
     }
+
 }
